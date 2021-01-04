7060c0342928cca36d070f4ddffee5bc4bbbe5e8
zhengvipin <>
2021-01-04 20:32:10 +0800

    抽离 plugins

---
+++ src/plugins/index.js
@@ -0,0 +1,5 @@
+import 'normalize.css/normalize.css' // A modern alternative to CSS resets
+import './element' // element-ui
+import '@/styles/index.scss' // global css
+import '@/icons' // icon
+import '@/permission' // permission control
