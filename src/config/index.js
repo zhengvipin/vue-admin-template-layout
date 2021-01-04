fdab4bbf70fc83cd918435c640dd9a6a47a0cf3b
zhengvipin <>
2021-01-04 20:35:21 +0800

    抽离 config

---
+++ src/config/index.js
@@ -0,0 +1,4 @@
+const permission = require('./permission')
+const settings = require('./settings')
+const constant = require('./constant')
+module.exports = { permission, settings, constant }
--- src/plugins/index.js
+++ src/plugins/index.js
@@ -2,4 +2,4 @@ import 'normalize.css/normalize.css' // A modern alternative to CSS resets
 import './element' // element-ui
 import '@/styles/index.scss' // global css
 import '@/icons' // icon
-import '@/permission' // permission control
+import '@/config/permission' // permission control
